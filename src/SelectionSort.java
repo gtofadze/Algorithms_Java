37236a3d8aa1828ab7a5477fbd8f53ddbc5258a4
Tofa <tofadze.1998@gmail.com>
2026-03-05 17:15:29 +0400

    added missing semicolon in SelectionSort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -3,7 +3,7 @@ public static void SelectionSort(int[] arr) {
     int len = arr.length;
 
     for (int i = 0; i < (len - 1); i++) {
-	int min = i
+	int min = i;
         for (int j = i + 1;  j < len; j++) {
 		if (arr[j] < arr[i]) {
 			min = j;
